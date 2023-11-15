89a5fabc4dceec307c50e54b2c03356d50ecd86b
FTAndy <fortheandy@gmail.com>
2023-11-15 12:55:25 +0800

    add cache

--- src/app/comedians/page.tsx
+++ src/app/comedians/page.tsx
@@ -2,6 +2,7 @@
 
 
 import Search from './components/Search'
+import { kv } from '@vercel/kv';
 import ComedianList from './components/ComedianList'
 import './page.scss'
 import type { Comedians, Comedian } from '@/types/comdian'
@@ -11,26 +12,38 @@ interface IComediansProps {
 
 // TODO: SSR
 async function getData<T>() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/comedians?page=0`)
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  const json = await res.json()
+  const data = await kv.get('api/comedians?page=0');
+  if (!data) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/comedians?page=0`)
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+  
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
+  
+    const json = await res.json()
 
-  return (json as {
-    data: T
-  })
+    kv.set('api/comedians?page=0', json, {
+      // 24 hour expire
+      ex: 60 * 60 * 24
+    });
+
+    return (json as T)
+  } else {
+    console.log('hit cache', data)
+    return (data as T)
+  }
+
 }
 
 
 const Comedians: React.FunctionComponent<IComediansProps> = async (props) => {
 
-  const {data} = await getData<Array<Comedian>>()
+  const {data} = await getData<{
+    data: Array<Comedian>
+  }>()
 
   // TODO: change to instant search
 
