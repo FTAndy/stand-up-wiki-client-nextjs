7a16ef8cf856cc9c0951ee3adf79cd3eaded8a0b
FTAndy <fortheandy@gmail.com>
2023-11-16 13:08:37 +0800

    cache data using fetch api instead of redis

--- src/app/comedians/page.tsx
+++ src/app/comedians/page.tsx
@@ -2,7 +2,7 @@
 
 
 import Search from './components/Search'
-import { kv } from '@vercel/kv';
+// import { kv } from '@vercel/kv';
 import ComedianList from './components/ComedianList'
 import './page.scss'
 import type { Comedians, Comedian } from '@/types/comdian'
@@ -12,29 +12,36 @@ interface IComediansProps {
 
 // TODO: SSR
 async function getData<T>() {
-  const data = await kv.get('api/comedians?page=0');
-  if (!data) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/comedians?page=0`)
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-  
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data')
+  // const data = await kv.get('api/comedians?page=0');
+  // if (!data) {
+
+  // server side fetch will have cache feature
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/comedians?page=0`, {
+    next: {
+      // cache data for each day
+      revalidate: 60 * 60 * 24
     }
-  
-    const json = await res.json()
+  })
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-    kv.set('api/comedians?page=0', json, {
-      // 24 hour expire
-      ex: 60 * 60 * 24
-    });
-
-    return (json as T)
-  } else {
-    console.log('hit cache', data)
-    return (data as T)
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
   }
+
+  const json = await res.json()
+
+  // kv.set('api/comedians?page=0', json, {
+  //   // 24 hour expire
+  //   ex: 60 * 60 * 24
+  // });
+
+  return (json as T)
+  // } else {
+  //   console.log('hit cache', data)
+  //   return (data as T)
+  // }
 
 }
 
@@ -44,6 +51,8 @@ const Comedians: React.FunctionComponent<IComediansProps> = async (props) => {
   const {data} = await getData<{
     data: Array<Comedian>
   }>()
+
+  console.log('render')
 
   // TODO: change to instant search
 
