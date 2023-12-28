03acdabcdb7944d7340ec424ef8d083a01ce3559
FTAndy <fortheandy@gmail.com>
2023-12-28 18:02:22 +0800

    bug fixes

--- src/app/(main)/profile/[id]/layout.tsx
+++ src/app/(main)/profile/[id]/layout.tsx
@@ -1,13 +1,8 @@
-import { ReactNode } from 'react';
-import Head from 'next/head';
 import Script from 'next/script'
 
-type LayoutProps = {
-  children: ReactNode;
-  title: string;
-};
-
-const Layout = ({ children, title }: LayoutProps) => {
+const Layout = ({ children }: {
+  children: React.ReactNode
+}) => {
   return (
     <>
       <Script src="https://cdn.jsdelivr.net/npm/hls.js@1"></Script>
