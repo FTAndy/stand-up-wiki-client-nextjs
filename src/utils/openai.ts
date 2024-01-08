3b641cdd8e6fd3b8812d136117df8df2ef4024cf
FTAndy <fortheandy@gmail.com>
2024-01-08 17:04:24 +0800

    add log

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -3,66 +3,65 @@ import 'dotenv/config'
 import OpenAI from 'openai'
 import type { MessageContentText } from 'openai/resources/beta/threads/messages/messages'
 
-const openai = new OpenAI({
+const config = {
   // https://api.openai-proxy.com
   ...(process.env.NODE_ENV === 'development' ? {
     baseURL: 'https://openai.wndbac.cn/v1'
   }: {}),
   apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-});
+}
+
+const openai = new OpenAI(config);
 
 export async function createThreadAndRunWithAssistant(assistantId: string) {
-  try {
-    const topics = ['Family and Relationships', 'Mental Health', 'Social Observations', 'Personal Anecdotes and Self-Deprecation:', 'Controversial or Taboo Topics']
+  console.log(config, 'config')
+  const topics = ['Family and Relationships', 'Mental Health', 'Social Observations', 'Personal Anecdotes and Self-Deprecation:', 'Controversial or Taboo Topics']
 
-    const index = Math.floor(Math.random() * topics.length)
+  const index = Math.floor(Math.random() * topics.length)
 
-    const {id: runId, thread_id} = await openai.beta.threads.createAndRun({
-      assistant_id: assistantId,
-      thread: {
-        messages: [
-          {
-            role: 'user',
-            content: `create a joke about ${topics[index]}`
-          }
-        ]      
-      }
-    })
-
-    await new Promise((resolve, reject) => {
-      let timeout = setInterval(async () => {
-        try {
-          const runTask = await openai.beta.threads.runs.retrieve(thread_id, runId)
-          if (runTask.status === 'completed') {
-            clearInterval(timeout)
-            resolve(runTask)
-          }        
-        } catch (error) {
-          clearInterval(timeout)
-          reject(error)
+  const {id: runId, thread_id} = await openai.beta.threads.createAndRun({
+    assistant_id: assistantId,
+    thread: {
+      messages: [
+        {
+          role: 'user',
+          content: `create a joke about ${topics[index]}`
         }
-      }, 500)
-    })
+      ]      
+    }
+  })
 
-  
-    const messageList = await openai.beta.threads.messages.list(thread_id, {
-      limit: 1
-    })
+  await new Promise((resolve, reject) => {
+    let timeout = setInterval(async () => {
+      try {
+        const runTask = await openai.beta.threads.runs.retrieve(thread_id, runId)
+        if (runTask.status === 'completed') {
+          clearInterval(timeout)
+          resolve(runTask)
+        }        
+      } catch (error) {
+        clearInterval(timeout)
+        reject(error)
+      }
+    }, 500)
+  })
 
-    const result = messageList.data[0].content[0] as MessageContentText
 
-    const respondMessageId = messageList.data[0].id
+  const messageList = await openai.beta.threads.messages.list(thread_id, {
+    limit: 1
+  })
 
-    const answer = result.text.value  
-    return {
-      answer,
-      thread_id,
-      respondMessageId
-    }  
-  } catch (error) {
-    console.log(error)
-    throw new Error('fetch open api error')
-  }
+  const result = messageList.data[0].content[0] as MessageContentText
+
+  const respondMessageId = messageList.data[0].id
+
+  const answer = result.text.value  
+  return {
+    answer,
+    thread_id,
+    respondMessageId
+  }  
+
 }
 
 export async function sendMessageToThread(threadId: string, message: string, assistant_id: string) {
