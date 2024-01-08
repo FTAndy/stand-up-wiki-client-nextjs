61a3bb9fe82bc0f4ada80749cece23fafecc9c96
FTAndy <fortheandy@gmail.com>
2024-01-08 15:42:57 +0800

    change env

--- src/utils/getPublicPath.ts
+++ src/utils/getPublicPath.ts
@@ -1,7 +1,7 @@
 export const getBaseUrl = () => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "development") {
     return "http://localhost:3000";
-  } else if (process.env.NODE_ENV === "production") {
+  } else if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
     return "https://standupwiki.com"
   }
   return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
