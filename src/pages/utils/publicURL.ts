6d16eabb64366d7c9809bcc057f91aca1c6794f3
FTAndy <fortheandy@gmail.com>
2024-01-08 15:27:37 +0800

    fix type error

--- src/pages/utils/publicURL.ts
+++ src/pages/utils/publicURL.ts
@@ -1,6 +1,8 @@
 export const getBaseUrl = () => {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000";
+  } else if (process.env.NODE_ENV === "production") {
+    return "https://standupwiki.com"
   }
   return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 };
